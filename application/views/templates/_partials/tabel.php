bc87df9c00e6fd804e05469ef657f7fe955e45a6
Afrizal <afrizalbagas.03@gmail.com>
2020-02-14 23:02:32 +0700

    lihat detail lokasi now success open in firefox browser

--- application/views/templates/_partials/tabel.php
+++ application/views/templates/_partials/tabel.php
@@ -6,9 +6,9 @@
             <th>User ID</th>
             <th>Absen Masuk</th>
             <th>Absen Keluar</th>
-            <th>Izin</th>
-            <th>Approve by</th>
-            <th>Action</th>
+            <th class="text-left">Izin</th>
+            <th class="text-center">Approve by</th>
+            <th class="text-center">Action</th>
         </tr>
     </thead>
     <tbody>
@@ -23,8 +23,8 @@
                 <td><?php echo $u->end_date ?></td>
                 <td><?php echo $u->ijin_pulang_cepat ?></td>
                 <td><?php echo $u->approve_by ?></td>
-                <td>
-                    <a href="javascriptBox:void(0)" id="jwpopupLink" style="color: #fd7e15; margin-right: 5px" data-toggle="tooltip" data-placement="top" title="Lihat Detail Lokasi">
+                <td class="text-center">
+                    <a href="javascript:void(0)" id="jwpopupLink" style="color: #fd7e15; margin-right: 5px" data-toggle="tooltip" data-placement="top" title="Lihat Detail Lokasi">
                         <span><img src="<?php echo base_url('assets/img/icons/map-marked.svg') ?>" width="25" height="25"></span>
                     </a>
                     <a id="delete" style="color: #fd7e15" data-toggle="tooltip" data-placement="top" title="Hapus Data">
